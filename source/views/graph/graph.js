8803eac3e60241300b3ba674e06e23b5615c95aa
Ihor Shalimov <i.shalimov@mobidev.biz>
2013-07-19 17:32:37 +0300

    li over the canvas

--- source/views/graph/graph.js
+++ source/views/graph/graph.js
@@ -17,7 +17,7 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
     onInitialize : function(){
         this.scrollBio.daysFromBirth = this.getDaysFromBirth();
         this.scrollBio.currentDay = this.scrollBio.daysFromBirth;
-        this.scrollBio.range = 1;
+        this.scrollBio.range = 2;
     },
 
     scrollBio : {
@@ -38,9 +38,21 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
                 };
 
 
-        this.$el.find('canvas').attr({width : this.$el.width(), height : this.$el.height()});
+        var canvas = this.$el.find('canvas').attr({width : this.$el.width(), height : this.$el.height()})[0];
         var self = this;
         var y = 0;
+
+        var visualRange = this.scrollBio.range * 2 + 1;
+        var daysList = this.$el.find('ul');
+        console.log(canvas.width);
+        for (var key=0; key < visualRange + 1; key++){
+            var li = $('<li/>').appendTo(daysList).css({
+                'width' : canvas.width / visualRange /1.5+ 'px',
+                'height' : canvas.height / 2 + 'px'
+            });
+        };
+
+        this.list = daysList.find('li');
 
         var callback = function(){
             y++;
@@ -56,12 +68,13 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
 //                }
                 //self.scrollBio.scrollSpeed = (self.scrollBio.scrollSpeed > 0 && self.scrollBio.currentDay < self.scrollBio.daysFromBirth) ? self.scrollBio.scrollSpeed : 0;
                 self.scrollBio.currentDay += self.scrollBio.scrollSpeed / 24;
-                self.draw(self.getBounds(new Date(1989, 2, 1, self.scrollBio.currentDay * 24), 3));
+                self.draw(self.getBounds(new Date(1989, 2, 1, self.scrollBio.currentDay * 24)));
                 y=0;
             }
 
             requestAF(callback);
         };
+
         requestAF(callback);
 
     },
@@ -69,7 +82,7 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
     swipeGraph : function(e){
         console.log(this.scrollBio);
 
-        if (e.originalEvent.swipe.speed !== 'Infinity') this.scrollBio.scrollSpeed = e.originalEvent.swipe.speed * 5 * (e.originalEvent.swipe.direction === 'right' ? -1 : 1);
+        if (e.originalEvent.swipe.speed !== 'Infinity') this.scrollBio.scrollSpeed = e.originalEvent.swipe.speed * 30 * (e.originalEvent.swipe.direction === 'right' ? -1 : 1);
      	/*function(t, b, c, d) {
 		    var ts=(t/=d)*t;
 		    var tc=ts*t;
@@ -93,44 +106,74 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
         var visualRange = this.scrollBio.range * 2 + 1;
         var halfHeight = canvas.height / 2;
         var halfWidth = canvas.width / 2;
+        var visualDayLength = canvas.width / visualRange;
 
         context.lineWidth = 2;
         context.beginPath();
-        for (var i = 0; i < 3; i++) {
-//            var begY = halfHeight - arr[i]*halfHeight + (arr[i]>0 ? 5 : -5);
-            var begX = 0;
-            var begY = halfHeight - arr[i]*halfHeight;
-            var endX = canvas.width;
-            var endY = halfHeight - arr[i+3]*halfHeight;
-            context.moveTo(begX, 5 + begY);
-            context.bezierCurveTo(begX + halfWidth, 5 + begY - 10 * arr[i+3], endX - halfHeight, 5 + endY - 10 * arr[i], endX, 5 + endY);
+
+//        for (var i = 0; i < arr.length; i++) {
+////            var begY = halfHeight - arr[i]*halfHeight + (arr[i]>0 ? 5 : -5);
+//            var begX = 0;
+//            var begY = halfHeight - arr[i]*halfHeight;
+//            var endX = canvas.width;
+//            var endY = halfHeight - arr[i+3]*halfHeight;
+//            context.moveTo(begX, 5 + begY);
+//            context.bezierCurveTo(begX + halfWidth, 5 + begY - 10 * arr[i+3], endX - halfHeight, 5 + endY - 10 * arr[i], endX, 5 + endY);
+//
+//        }
+
+
+        for (var i = 0; i < arr.length-1; i++) {
+            //console.log(arr[i]);
+
+            for (var bio = 0; bio<3; bio++) {
+//              var begY = halfHeight - arr[i]*halfHeight + (arr[i]>0 ? 5 : -5);
+                context.beginPath();
+                var begX = visualDayLength * i;
+                var begY = halfHeight - arr[i][bio]*halfHeight;
+                var endX = (i+1) * visualDayLength;
+                var endY = halfHeight - arr[i+1][bio]*halfHeight;
+                context.moveTo(begX, begY);
+                //context.bezierCurveTo(begX + halfWidth, 5 + begY - 10 * arr[i+3], endX - halfHeight, 5 + endY - 10 * arr[i], endX, 5 + endY);
+                context.lineTo(endX, endY);
+                context.strokeStyle = colors[bio];
+                context.closePath();
+                context.stroke();
+            }
 
         }
+        context.strokeStyle = 'rgba(50,50,50,1)';
+        context.stroke();
 
-        for (var days = 0; days<visualRange; days++) {
-            var begin = canvas.width - (this.scrollBio.currentDay - Math.floor(this.scrollBio.currentDay)) * canvas.width / visualRange;
-            context.moveTo(begin - days*canvas.width/visualRange, 0);
-            context.lineTo(begin - days*canvas.width/visualRange, canvas.height);
-            context.fillText(Math.floor(this.scrollBio.currentDay - days + 3), begin - days*canvas.width/visualRange, 20);
+        context.beginPath();
+        for (var days = 0; days<visualRange+1; days++) {
+            var begin = canvas.width - (this.scrollBio.currentDay - Math.floor(this.scrollBio.currentDay)) * visualDayLength;
+            context.moveTo(begin - days*visualDayLength, 0);
+            context.lineTo(begin - days*visualDayLength, canvas.height);
+            //context.fillText(Math.floor(this.scrollBio.currentDay - days + this.scrollBio.range + 1), begin - days*visualDayLength, 20);
+            //this.list[days].style.left = begin - days*visualDayLength + 'px';
+            //$(this.list[days]).text(Math.floor(this.scrollBio.currentDay - days + this.scrollBio.range + 1));
+
+            //console.log(this.list[days].style.left);
         }
         context.moveTo(0, halfHeight);
         context.lineTo(canvas.width, halfHeight);
         context.strokeStyle = 'rgba(50,50,50,0.2)';
         context.stroke();
 
-        for (var i = 0; i < 3; i++) {
-            var begX = 0;
-            var begY = halfHeight - arr[i]*halfHeight;
-            var endX = canvas.width;
-            var endY = halfHeight - arr[i+3]*halfHeight;
-
-            context.beginPath();
-            context.moveTo(begX, begY);
-            context.bezierCurveTo(begX + halfWidth, begY - 5 * arr[i+3], endX - halfHeight, endY - 5 * arr[i], endX, endY);
-            context.strokeStyle = colors[i];
-            ///context.closePath();
-            context.stroke();
-        }
+//        for (var i = 0; i < 3; i++) {
+//            var begX = 0;
+//            var begY = halfHeight - arr[i]*halfHeight;
+//            var endX = canvas.width;
+//            var endY = halfHeight - arr[i+3]*halfHeight;
+//
+//            context.beginPath();
+//            context.moveTo(begX, begY);
+//            context.bezierCurveTo(begX + halfWidth, begY - 5 * arr[i+3], endX - halfHeight, endY - 5 * arr[i], endX, endY);
+//            context.strokeStyle = colors[i];
+//            ///context.closePath();
+//            context.stroke();
+//        }
 
 
 
@@ -138,25 +181,33 @@ RAD.view("view.graph", RAD.Blanks.View.extend({
 
     getBounds : function(middleDay){
         var birth = new Date(1989, 2, 1);
-        var b = new Date();
+        //var b = new Date();
 
         middleDay = middleDay || b;
         var range = this.scrollBio.range;
         var dates = [];
-        dates.push(new Date(middleDay.getTime() - this.scrollBio.dayLength*range));
-        dates.push(new Date(middleDay.getTime() + this.scrollBio.dayLength*range));
+        var middleDayTimestamp = middleDay.getTime();
+        for (var d=-this.scrollBio.range - 1; d<= this.scrollBio.range + 1; d++) {
+            dates.push(new Date(middleDayTimestamp + this.scrollBio.dayLength*d));
+        }
+//        dates.push(new Date(middleDay.getTime() - this.scrollBio.dayLength*range));
+//        dates.push(new Date(middleDay.getTime() + this.scrollBio.dayLength*range));
         var periods = [23, 28, 33];
 
         var res = [];
 
         for (var date in dates)
         {
-            for (var per in periods){
-                var days = (dates[date] - birth)/this.scrollBio.dayLength;
-                res.push(Math.sin(2*Math.PI*days/periods[per]));
-            }
-        }
+            var day = [];
+            ///console.log(dates[date] - birth);
 
+            for (var per in periods){
+                var d = (dates[date] - birth)/this.scrollBio.dayLength;
+                day[per] = (Math.sin(2*Math.PI*d/periods[per]));
+            }
+            res.push(day);
+        }
+        //console.log(res.length);
         return res;
     },
 
